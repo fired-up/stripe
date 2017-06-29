05fb1c4872a077b806c0a3e4b5544fdc69de3659
Kyle Hotchkiss <kyle@kylehotchkiss.com>
2017-06-29 16:27:27 -0700

    Write some ideas for how to get started

---
+++ functions/library/stripe.js
@@ -0,0 +1,9 @@
+// Private
+//getCustomer - get customer from stripe
+//createCustomer - create customer on stripe
+//updateCustomer - update customer on stripe
+//verifyPlan - find or create or force update plan
+
+// Public
+//single
+//recurring
---
+++ functions/library/webhook.js
@@ -0,0 +1,30 @@
+
+
+exports.dispatch = function( req, res ) {
+    var customer, subscription;
+
+    var stripeEvent = req.body;
+    var transaction = stripeEvent.data.object;
+
+
+    if ( stripeEvent.type === "charge.refunded" || stripeEvent.type === "charge.dispute.closed" ) {
+        //
+        // Refund or dispute successfully processed
+        //
+
+
+
+    } else if ( stripeEvent.type === "invoice.payment_succeeded" ) {
+        //
+        // Recurring Donations successfully made
+        //
+
+
+    } else if ( stripeEvent.type === "customer.subscription.created" ) {
+
+    } else if ( stripeEvent.type === "customer.subscription.deleted" ) {
+
+    }
+
+    res.send(200);
+};
